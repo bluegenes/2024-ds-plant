a5887b5ab559d2c4cc38a2df2dd1d0c78294ba1c
N. Tessa Pierce-Ward <ntpierce@gmail.com>
2024-12-10 12:45:12 -0800

    upd

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #sourmash scripts gbsketch Reference_genomes_plants_genbank.directsketch.head10.csv -c 1 -r 1 -n 1 --genomes-only -o Reference_genomes_plants_genbank.directsketch.head10.zip --failed Reference_genomes_plants_genbank.directsketch.head10.failed.csv --checksum-fail Reference_genomes_plants_genbank.directsketch.head10.checksum-fail.csv
 
 # run full db, with batching
-#sourmash scripts gbsketch Reference_genomes_plants_genbank.directsketch.csv -c 1 -r 1 -n 1 --genomes-only -o Reference_genomes_plants_genbank.directsketch.zip --failed Reference_genomes_plants_genbank.directsketch.failed.csv --checksum-fail Reference_genomes_plants_genbank.directsketch.checksum-fail.csv
+#sourmash scripts gbsketch Reference_genomes_plants_genbank.directsketch.csv -c 1 -r 1 -n 2 --genomes-only -o Reference_genomes_plants_genbank.directsketch.zip --failed Reference_genomes_plants_genbank.directsketch.failed.csv --checksum-fail Reference_genomes_plants_genbank.directsketch.checksum-fail.csv --batch-size 200
 
 # cat to single db
 #sourmash sig cat Reference_genomes_plants_genbank.directsketch.*.zip -o Reference_genomes_plants_genbank.directsketch.zip
